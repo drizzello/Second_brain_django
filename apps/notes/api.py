c6b97c523d34f1442af31cda39f43a919d519bc0
Drizzello <d.rizzello@studenti.unipi.it>
2026-05-22 16:06:18 +0200

    Add user ownership verification to note operations in API

--- apps/notes/api.py
+++ apps/notes/api.py
@@ -3,8 +3,9 @@ from ninja import NinjaAPI
 from apps.notes.services import create_note
 from apps.notes.schema import NoteCreateIn, NoteOut, NotFoundSchema, NoteUpdateIn
 from apps.notes.models import Note
+from ninja.security import SessionAuth
 
-api = NinjaAPI()
+api = NinjaAPI(auth=SessionAuth())
 
 @api.get("/notes", response=list[NoteOut])
 def NoteList(request):
@@ -22,7 +23,7 @@ def CreateNote(request, payload: NoteCreateIn):
 @api.put("/notes/update/{note_id}", response={200: NoteOut, 404: NotFoundSchema})
 def UpdateNote(request, note_id: int, data: NoteUpdateIn):
     try:
-        note = Note.objects.get(pk=note_id)
+        note = Note.objects.get(pk=note_id, owner=request.user)
         for attribute, value in data.dict().items():
             setattr(note, attribute, value)
         note.save()
@@ -33,15 +34,15 @@ def UpdateNote(request, note_id: int, data: NoteUpdateIn):
 @api.get("/notes/{note_id}", response={200: NoteOut, 404: NotFoundSchema})
 def NoteDetail(request, note_id):
     try: 
-        nota = Note.objects.get(pk=note_id)
+        nota = Note.objects.get(pk=note_id, owner=request.user)
         return 200, nota
     except Note.DoesNotExist as e:
         return 404, {"message": "could not find note"}
     
 @api.delete("/notes/delete/{note_id}", response={200: None, 404: NotFoundSchema})
-def delete_track(request, note_id: int):
+def NoteDelete(request, note_id: int):
     try:
-        note = Note.objects.get(pk=note_id)
+        note = Note.objects.get(pk=note_id, owner=request.user)
         note.delete()
         return 200
     except Note.DoesNotExist as e:
